d0c8675c6f49eb94428805ac27cedd3eacb97cba
Hongtao <acarlhong@gmail.com>
2019-11-29 15:05:19 +0800

    Updates

---
+++ precache-manifest.6f85942e423306a84d7fe4506833cf78.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfb759e6aa768a6587713b34f357aae2",
+    "url": "/home_io_site/static/media/section-3-partner-mobile.dfb759e6.png"
+  },
+  {
+    "revision": "e51ee6f09e3939809025",
+    "url": "/home_io_site/static/css/main.46461d49.chunk.css"
+  },
+  {
+    "revision": "0409a891cddaa876237a",
+    "url": "/home_io_site/static/js/runtime~main.25af299c.js"
+  },
+  {
+    "revision": "6f87c0a38954bf07ac15",
+    "url": "/home_io_site/static/js/2.32cecfc8.chunk.js"
+  },
+  {
+    "revision": "9c8bc9ff8c041872612a",
+    "url": "/home_io_site/static/js/3.15fb8609.chunk.js"
+  },
+  {
+    "revision": "a11ad30b716b68505e64b535499d3c53",
+    "url": "/home_io_site/static/media/section-2-alex.a11ad30b.png"
+  },
+  {
+    "revision": "a1e6c2b792db0d5034e7",
+    "url": "/home_io_site/static/js/4.83faa914.chunk.js"
+  },
+  {
+    "revision": "1e75360a1680d990530f",
+    "url": "/home_io_site/static/js/5.9176c587.chunk.js"
+  },
+  {
+    "revision": "c411f608b82aaf80d9fa1ac9e316b3ce",
+    "url": "/home_io_site/static/media/Eina01Regular.c411f608.otf"
+  },
+  {
+    "revision": "1dcabfe91ab841d8bba2737620b29ddc",
+    "url": "/home_io_site/static/media/Eina01SemiBold.1dcabfe9.otf"
+  },
+  {
+    "revision": "9f04d8e29fb2c2e80892e10deea9608e",
+    "url": "/home_io_site/static/media/lng.9f04d8e2.svg"
+  },
+  {
+    "revision": "e0b783915d8fa4c85c6486f9ebcf045f",
+    "url": "/home_io_site/static/media/lng-light.e0b78391.svg"
+  },
+  {
+    "revision": "6b9d15b710902b0a00afd726b7c5a7d5",
+    "url": "/home_io_site/static/media/logo.6b9d15b7.svg"
+  },
+  {
+    "revision": "3511f06a64e831a63ffd23a8d9e70337",
+    "url": "/home_io_site/static/media/logo-light.3511f06a.svg"
+  },
+  {
+    "revision": "bfc2989f3b125c77bc1a509e367d4551",
+    "url": "/home_io_site/static/media/section-1-evoland.bfc2989f.png"
+  },
+  {
+    "revision": "e51ee6f09e3939809025",
+    "url": "/home_io_site/static/js/main.12b91a6b.chunk.js"
+  },
+  {
+    "revision": "77952c1b500e8d0b48b57ba81e11596a",
+    "url": "/home_io_site/static/media/section-3-bg-mobile.77952c1b.png"
+  },
+  {
+    "revision": "3110379b583a895f915e4f329c94b417",
+    "url": "/home_io_site/static/media/section-3-partner.3110379b.png"
+  },
+  {
+    "revision": "37ebb3f1df30d7a6f97747f0bb88480a",
+    "url": "/home_io_site/static/media/section-1-evoland-hover.37ebb3f1.png"
+  },
+  {
+    "revision": "064b49bf6262e009eead90f0cb2cfa7b",
+    "url": "/home_io_site/static/media/section-1-itering-hover.064b49bf.png"
+  },
+  {
+    "revision": "8688e3390e093e6c8a2df03b2087ac8f",
+    "url": "/home_io_site/static/media/section-1-darwinia-hover.8688e339.png"
+  },
+  {
+    "revision": "5e0868979459bf1479549dcf775cf0c9",
+    "url": "/home_io_site/static/media/home-banner-mobile.5e086897.png"
+  },
+  {
+    "revision": "3aaeb39d5b939acfc5820000fb850970",
+    "url": "/home_io_site/static/media/section-4-location-mobile.3aaeb39d.png"
+  },
+  {
+    "revision": "43169463527694f997e0d0eeabae4a3b",
+    "url": "/home_io_site/static/media/section-1-nft-hover.43169463.png"
+  },
+  {
+    "revision": "b6e450d617aeafe2ae14941a1e17791a",
+    "url": "/home_io_site/static/media/section-3-bg.b6e450d6.png"
+  },
+  {
+    "revision": "612544210ce80b85a93b6c70c23b0528",
+    "url": "/home_io_site/static/media/home-banner.61254421.png"
+  },
+  {
+    "revision": "d55ae6cf1f4d596316f4f2273b24de61",
+    "url": "/home_io_site/static/media/section-4-location.d55ae6cf.png"
+  },
+  {
+    "revision": "f67491f9239029bcae5342dfd903176b",
+    "url": "/home_io_site/static/media/section-2-denny.f67491f9.png"
+  },
+  {
+    "revision": "a1e6c2b792db0d5034e7",
+    "url": "/home_io_site/static/css/4.7451c1c9.chunk.css"
+  },
+  {
+    "revision": "f7ca7fbeb5b441376f38e6726b7d69be",
+    "url": "/home_io_site/index.html"
+  }
+];
